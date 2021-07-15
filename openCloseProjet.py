0e9e40e0486b692ffff102648ddbd64462c727ba
jlienard <jefferson.lienard@gmail.com>
2021-07-15 11:26:19 +0200

    Update de openCloseProjet

--- openCloseProjet.py
+++ openCloseProjet.py
@@ -4,10 +4,12 @@ def openProjet():
 	os.system('./projet & > test.file')
 
 def closeProjet():
-	os.system('echo ALLO')
-	os.system('echo $(ps -e | grep projet | cut -d" " -f1)')
 	os.system('sudo kill -9 $(ps -e | grep projet | cut -d" " -f1)')
-	os.system('echo $(ps -e | grep projet | cut -d" " -f1)')
-
+	os.system('if [ $? -eq 0 ] 
+		then 
+			echo Process killed successfully
+		else 
+			echo 'Process could not be killed' 
+		fi')
 
 
