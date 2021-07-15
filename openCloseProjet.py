d69cdde02635394142e10ffe09d2bff128b628bc
jlienard <jefferson.lienard@gmail.com>
2021-07-15 11:42:01 +0200

    Update de openCloseProjet

--- openCloseProjet.py
+++ openCloseProjet.py
@@ -6,8 +6,7 @@ def openProjet():
 def closeProjet():
 	exit_code = os.system('sudo kill -9 $(ps -e | grep projet | cut -d" " -f1)')
 	if exit_code == 0:
-		print("Process killed successfully")
+		os.system('echo Process killed successfully')
 	else:
-		print("exit code : " + exit_code ) 
-		print("Process could not be killed")
+		os.system('echo Process could not be killed')
 
